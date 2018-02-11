f7b10244e3fb6c2e2d133b52169ff12814e9f436
Aditya Pratap Singh <adisinghrajput@gmail.com>
2018-02-11 15:59:10 +0100

    Add snippet for sync and async event emittor

--- src/event-emittor.js
+++ src/event-emittor.js
@@ -1,17 +1,34 @@
 const EventEmitter = require('events').EventEmitter;
 
+//#region Synchronous EventEmitter
 const john = new EventEmitter();
+const jane = new EventEmitter();
+
+jane.emit('greet', `Jane`);
+john.emit('greet', `John`);
 
 john.on('greet', (person) => {
     console.log(`Hello from ${person}`);
 });
 
-const jane = new EventEmitter();
-
 jane.on('greet', (message) => {
     console.log('>>>>>>>>>>>', message, '<<<<<<<<<<<<<');
 });
+//#endregion
 
-jane.emit('greet', `Jane`);
-john.emit('greet', `John`);
+//#region Async EventEmitter
+const johnAsync = new EventEmitter();
+const janeAsync = new EventEmitter();
+
+setImmediate(() => janeAsync.emit('greet', `Jane`));
+setImmediate(() => johnAsync.emit('greet', `John`));
+
+johnAsync.on('greet', (person) => {
+    console.log(`Hello from ${person}`);
+});
+
+janeAsync.on('greet', (message) => {
+    console.log('>>>>>>>>>>>', message, '<<<<<<<<<<<<<');
+});
+//#endregion
 
